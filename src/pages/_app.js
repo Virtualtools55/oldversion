52d16dcab54cd261384d39a930aee16fee6d3205
Virtualtools55 <tools.designwebdev@gmail.com>
2024-06-15 14:30:53 +0530

    some code added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
 import "@/styles/globals.css";
-
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/footer";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+
+
+  return <>
+  <Navbar/>
+
+  <Component {...pageProps} />;
+  
+  <Footer/>
+  </>
+  
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import IntroContainer from "@/components/introContainer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-     
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`} >
+     <IntroContainer/>
     </main>
   );
 }
